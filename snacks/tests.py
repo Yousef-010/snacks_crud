9b6327553ed19f71d72b0053cc9ffaa1efe829fc
Yousef Al Farwan <yousefsyria304@gmail.com>
2022-07-28 16:28:13 +0300

    update: All tests passed

--- snacks/tests.py
+++ snacks/tests.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 
 from .models import Snack
 
@@ -11,28 +11,28 @@ class SnackTests(TestCase):
         create a tester user
         """
         self.user = get_user_model().objects.create_user(
-            username="tester", email="tester@email.com", password="pass"
+            username="tester", email="tester@tester.com", password="12345test"
         )
         # create a record in the table
         self.snack = Snack.objects.create(
-            title="pickle",
+            title="Potato",
             purchaser=self.user,
-            description="Salty snack",
+            description="Good Snack",
         )
 
     def test_string_representation(self):
         """
         Tests string representation
         """
-        self.assertEqual(str(self.snack), "pickle")
+        self.assertEqual(str(self.snack), "Potato")
 
     def test_snack_content(self):
         """
         Test the record's data , title, purchaser and description
         """
-        self.assertEqual(f"{self.snack.title}", "pickle")
+        self.assertEqual(f"{self.snack.title}", "Potato")
         self.assertEqual(f"{self.snack.purchaser}", "tester")
-        self.assertEqual(f"{self.snack.description}", "Salty snack")
+        self.assertEqual(f"{self.snack.description}", "Good Snack")
 
     def test_snack_list_view(self):
         """
@@ -40,7 +40,7 @@ class SnackTests(TestCase):
         """
         response = self.client.get(reverse("snack_list"))
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "pickle")
+        self.assertContains(response, "Potato")
         self.assertTemplateUsed(response, "snack_list.html")
 
     def test_snack_detail_view(self):
@@ -69,6 +69,7 @@ class SnackTests(TestCase):
         )
 
         self.assertRedirects(response, reverse("snack_detail", args="2"))
+        self.assertTemplateUsed(response, 'snack_detail.html')
         self.assertContains(response, "Details about Chocolate")
 
     def test_snack_update_view_redirect(self):
@@ -92,3 +93,30 @@ class SnackTests(TestCase):
         """
         response = self.client.get(reverse("snack_delete", args="1"))
         self.assertEqual(response.status_code, 200)
+
+    def test_snack_create_template(self):
+        """
+        test the used template for the snack_create
+        """
+        url = reverse("snack_create")
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, "snack_create.html")
+        self.assertTemplateUsed(response, "base.html")
+
+    def test_snack_update_template(self):
+        """
+        test the used template for the snack_update
+        """
+        url = reverse("snack_update", args=[1])
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, "snack_update.html")
+        self.assertTemplateUsed(response, "base.html")
+
+    def test_snack_delete_template(self):
+        """
+        test the used template for the snack_delete
+        """
+        url = reverse("snack_delete", args=[1])
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, "snack_delete.html")
+        self.assertTemplateUsed(response, "base.html")
